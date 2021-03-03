6b0b14eb4e0ea16c1cd475a5635a1c963ab5ac51
taureandyernv <tdyer@nvidia.com>
2021-03-03 11:29:58 -0800

    Update Colab Install script for 0.18 and 0.19
    - updated rapids-colab script and env-check
    - added copy_libs.py with Randy's updated script

--- colab/rapids-colab.sh
+++ colab/rapids-colab.sh
@@ -1,126 +1,126 @@
 #!/bin/bash
 
-set -eu
+MULT="100"
+STABLE=18
+NIGHTLIES=19
+LOWEST=17
 
+CTK_VERSION=11.0
+
+RAPIDS_VERSION="0.$STABLE"
+RAPIDS_RESULT=$STABLE
+ 
 echo "PLEASE READ"
 echo "********************************************************************************************************"
-echo "Colab Notebooks Migration Notice:"
-echo " "
-echo "We have changed the location of the Colab script to our new CSP utilties repo."
-echo "We are also dropping support for versions below 0.11, as we are on 0.13 nightlies"
-echo " "
-echo "Not all Colab notebooks are updated (like personal Colabs) and/or are using outdated versions of RAPIDS,"
-echo "so we HIGHLY encourage users to update their scripts and Colab notebooks both with the new link AND with "
-echo "to the new API.  Otherwise, your code MAY BREAK"
-echo " "
-echo "This notice will disappear/be updated on our next release."
-echo " "
-echo "Please enter in the box your desired RAPIDS version (ex: '0.11' or '0.12', between 0.11 to 0.13, without the quotes)"
-echo "and hit Enter. If you need stability, use 0.12. If you want bleeding edge, use our nightlies (0.13), but things can break."
+echo "Changes:"
+echo "1. IMPORTANT CHANGES: RAPIDS on Colab now runs our STABLE and NIGHTLY versions (0.$STABLE and 0.$NIGHTLIES)."
+echo "2. IMPORTANT NOTICE: CuGraph's Louvain requires a Volta+ GPU (T4, V100).  If you get a P4 or P100 and intend to use Louvain, please FACTORY RESET your instance and try to get a compatible GPU"
+echo "3. Default stable version is now 0.$STABLE.  Nightly is now 0.$NIGHTLIES."
+echo "3. You can declare your RAPIDSAI version as a CLI option and skip the user prompts (ex: '0.$STABLE' or '0.$NIGHTLIES', between 0.$LOWEST to 0.$NIGHTLIES, without the quotes): "
+echo '        "!bash rapidsai-csp-utils/colab/rapids-colab.sh <version/label>"'
+echo "        Examples: '!bash rapidsai-csp-utils/colab/rapids-colab.sh 0.$STABLE', or '!bash rapidsai-csp-utils/colab/rapids-colab.sh stable', or '!bash rapidsai-csp-utils/colab/rapids-colab.sh s'"
+echo "                  '!bash rapidsai-csp-utils/colab/rapids-colab.sh 0.$NIGHTLIES, or '!bash rapidsai-csp-utils/colab/rapids-colab.sh nightly', or '!bash rapidsai-csp-utils/colab/rapids-colab.sh n'"
+echo "Enjoy using RAPIDS!  If you have any issues with or suggestions for RAPIDSAI on Colab, please create a bug request on https://github.com/rapidsai/rapidsai-csp-utils/issues/new."
+echo "For a near instant entry into a RAPIDS Library experience, or if we haven't fixed a fatal issue yet, please use https://app.blazingsql.com/.  Thanks!"
 
-read RAPIDS_VERSION
-MULT="100"
 
-#conditions accidental inputs
-RAPIDS_RESULT=$(awk '{print $1*$2}' <<<"${RAPIDS_VERSION} ${MULT}")
-#echo $RAPIDS_RESULT
-if (( $RAPIDS_RESULT >=13 )) ;then
-  RAPIDS_VERSION="0.13"
-  RAPIDS_RESULT=13
-  echo "RAPIDS Version modified to 0.13 nightlies"
-elif (( $RAPIDS_RESULT <= 11 )) ;then
-  RAPIDS_VERSION="0.11"
-  RAPIDS_RESULT=11
-  echo "RAPIDS Version modified to 0.11 stable"
-fi
-#echo $RAPIDS_VERSION
-if (( $RAPIDS_RESULT >= 11 )) ;then
-  echo "Please COMPARE the \"SCRIPT TO COPY\" with the code in the above cell.  If they are the same, just type any key.  If not, do steps 2-4. "
-  echo " "
-  echo "SCRIPT TO COPY: "
-  echo "!git clone https://github.com/rapidsai/rapidsai-csp-utils.git"
-  echo "!bash rapidsai-csp-utils/colab/rapids-colab.sh"
-  echo "import sys, os"
-  echo "dist_package_index = sys.path.index('/usr/local/lib/python3.6/dist-packages')"
-  echo "sys.path = sys.path[:dist_package_index] + ['/usr/local/lib/python3.6/site-packages'] + sys.path[dist_package_index:]"
-  echo "sys.path"
-  echo "exec(open('rapidsai-csp-utils/colab/update_modules.py').read(), globals())"
-  echo "********************************************************************************************************"
-  echo "Do you have the above version of the script running in your cell? (Y/N).  "
-  echo "Please note: If you do not positively affirm with 'Y' or 'y', the script show you have to rectify that issue and then stop the RAPIDS install,"
-  echo "without installing RAPIDS, but will still continue update some packages"
-  read response
-  if [ $response == "Y" ] || [ $response == "y" ] ;then
-    echo "Continuing with RAPIDS install"
-  else
-    echo "Please do the following:"
-    echo "1. STOP cell execution" 
-    echo "2. CUT and PASTE the script below into the cell you just ran "
-    echo "3. RERUN the cell"
-    echo " "
-    echo "SCRIPT TO COPY:"
-    echo "!git clone https://github.com/rapidsai/rapidsai-csp-utils.git"
-    echo "!bash rapidsai-csp-utils/colab/rapids-colab.sh"
-    echo "import sys, os"
-    echo "dist_package_index = sys.path.index('/usr/local/lib/python3.6/dist-packages')"
-    echo "sys.path = sys.path[:dist_package_index] + ['/usr/local/lib/python3.6/site-packages'] + sys.path[dist_package_index:]"
-    echo "sys.path"
-    echo "exec(open('rapidsai-csp-utils/colab/update_modules.py').read(), globals())"
-    echo "********************************************************************************************************"
-    echo "Please COPY the above code and RERUN the cell"
-    exit 0
-  fi
-else
-  echo "You may not have to change anything.  All versions of our script should work with this version of Colab"
-fi
+install_RAPIDS () {
+    echo "Checking for GPU type:"
+    python rapidsai-csp-utils/colab/env-check.py
 
-echo "Checking for GPU type:"
-python rapidsai-csp-utils/colab/env-check.py
+    if [ ! -f Miniconda3-4.5.4-Linux-x86_64.sh ]; then
+        echo "Removing conflicting packages, will replace with RAPIDS compatible versions"
+        # remove existing xgboost and dask installs
+        pip uninstall -y dask distributed xgboost pyarrow numba llvmlite PySocks requests six urllib3 cffi
 
-if [ ! -f Miniconda3-4.5.4-Linux-x86_64.sh ]; then
-    echo "Removing conflicting packages, will replace with RAPIDS compatible versions"
-    # remove existing xgboost and dask installs
-    pip uninstall -y xgboost dask distributed
+        # intall miniconda
+        echo "Installing conda"
+        wget -nc https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        chmod +x Miniconda3-latest-Linux-x86_64.sh
+        bash ./Miniconda3-latest-Linux-x86_64.sh -b -f -p /usr/local
 
-    # intall miniconda
-    echo "Installing conda"
-    wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
-    chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
-    bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
-    
-    #Installing another conda package first something first seems to fix https://github.com/rapidsai/rapidsai-csp-utils/issues/4
-    conda install -y --prefix /usr/local -c conda-forge openssl python=3.6
-    
-    if (( $RAPIDS_RESULT == 13 )) ;then #Newest nightly packages.  UPDATE EACH RELEASE!
-    echo "Installing RAPIDS $RAPIDS_VERSION packages from the nightly release channel"
-    echo "Please standby, this will take a few minutes..."
-    # install RAPIDS packages
-        conda install -y --prefix /usr/local \
-                -c rapidsai-nightly/label/xgboost -c rapidsai-nightly -c nvidia -c conda-forge \
-                python=3.6 cudatoolkit=10.0 \
-                cudf=$RAPIDS_VERSION cuml cugraph gcsfs pynvml cuspatial xgboost\
-                dask-cudf
-    else #Stable packages requiring PyArrow 0.15
-        echo "Installing RAPIDS $RAPIDS_VERSION packages from the stable release channel"
+        # pin python3.7
+        echo "python 3.7.*" > /usr/local/conda-meta/pinned
+		
+		
+		# Install RAPIDSAI Libraries
+        if (( $RAPIDS_RESULT == $NIGHTLIES )) ;then  # Newest nightly packages.  UPDATE EACH RELEASE!
+        echo "Installing RAPIDS $RAPIDS_VERSION packages from the nightly release channel"
         echo "Please standby, this will take a few minutes..."
         # install RAPIDS packages
-        conda install -y --prefix /usr/local \
-            -c rapidsai/label/main -c rapidsai -c nvidia -c conda-forge -c defaults\
-            python=3.6 cudatoolkit=10.0 \
-            cudf=$RAPIDS_VERSION cuml cugraph cuspatial gcsfs pynvml xgboost=1.0.0dev.rapidsai$RAPIDS_VERSION\
-            dask-cudf
+            conda install -y --prefix /usr/local \
+                -c rapidsai-nightly/label/xgboost -c rapidsai-nightly -c nvidia -c conda-forge -c defaults \
+                python=3.7 cudatoolkit=$CTK_VERSION \
+                rapids=$RAPIDS_VERSION \
+                llvmlite gcsfs openssl
+        elif (( $RAPIDS_RESULT == 13 )) ; then
+            echo "Installing RAPIDS $RAPIDS_VERSION packages from the stable release channel"
+            echo "Please standby, this will take a few minutes..."
+            # install RAPIDS packages
+            conda install -y --prefix /usr/local \
+                -c rapidsai -c nvidia -c conda-forge -c defaults \
+                python=3.7 cudatoolkit=$CTK_VERSION \
+                rapids=$RAPIDS_VERSION \
+                llvmlite gcsfs openssl
+        else  # Stable packages 0.18 uses xgboost 1.3.3
+            echo "Installing RAPIDS $RAPIDS_VERSION packages from the stable release channel"
+            echo "Please standby, this will take a few minutes..."
+            # install RAPIDS packages
+            conda install -y --prefix /usr/local \
+                -c rapidsai/label/main -c rapidsai -c nvidia -c conda-forge -c defaults \
+                python=3.7 cudatoolkit=$CTK_VERSION \
+                rapids=$RAPIDS_VERSION xgboost=1.3.3dev.rapidsai0.18 \
+                llvmlite gcsfs openssl
+        fi
+          
+        echo "Copying shared object files to /usr/lib"
+        # copy .so files to /usr/lib, where Colab's Python looks for libs
+        python rapidsai-csp-utils/colab/copy_libs.py
     fi
-      
-    echo "Copying shared object files to /usr/lib"
-    # copy .so files to /usr/lib, where Colab's Python looks for libs
-    cp /usr/local/lib/libcudf.so /usr/lib/libcudf.so
-    cp /usr/local/lib/librmm.so /usr/lib/librmm.so
-    cp /usr/local/lib/libnccl.so /usr/lib/libnccl.so
-    echo "Copying RAPIDS compatible xgboost"
-    cp /usr/local/lib/libxgboost.so /usr/lib/libxgboost.so
-fi
 
-echo ""
-echo "************************************************"
-echo "Your Google Colab instance has RAPIDS installed!"
-echo "************************************************"
+    echo ""
+    echo "************************************************"
+    echo "Your Google Colab instance has RAPIDS installed!"
+    echo "************************************************"
+}
+
+rapids_version_check () {
+    
+  if  [ $RESPONSE == "NIGHTLY" ]|| [ $RESPONSE == "nightly" ]  || [ $RESPONSE == "N" ] || [ $RESPONSE == "n" ] ; then
+    RAPIDS_VERSION="0.$NIGHTLIES"
+    RAPIDS_RESULT=$NIGHTLIES
+    echo "Starting to prep Colab for install RAPIDS Version $RAPIDS_VERSION nightly"
+  elif [ $RESPONSE == "STABLE" ]|| [ $RESPONSE == "stable" ]  || [ $RESPONSE == "S" ] || [ $RESPONSE == "s" ] ; then
+    RAPIDS_VERSION="0.$STABLE"
+    RAPIDS_RESULT=$STABLE
+    echo "Starting to prep Colab for install RAPIDS Version $RAPIDS_VERSION stable"
+  else
+    RAPIDS_RESULT=$(awk '{print $1*$2}' <<<"${RESPONSE} ${MULT}")
+    if (( $RAPIDS_RESULT > $NIGHTLIES )) ; then
+      RAPIDS_VERSION="0.$NIGHTLIES"
+      RAPIDS_RESULT=$NIGHTLIES
+      echo "RAPIDS Version modified to $RAPIDS_VERSION nightly"
+    elif (($RAPIDS_RESULT < $LOWEST)) ; then
+      RAPIDS_VERSION="0.$LOWEST"
+      RAPIDS_RESULT=$LOWEST
+      echo "RAPIDS Version modified to $RAPIDS_VERSION stable"
+    elif (($RAPIDS_RESULT >= $LOWEST)) &&  (( $RAPIDS_RESULT <= $NIGHTLIES )) ; then
+      RAPIDS_VERSION="0.$RAPIDS_RESULT"
+      echo "RAPIDS Version to install is $RAPIDS_VERSION"
+    else
+      echo "You've entered and incorrect RAPIDS version.  please make the necessary changes and try again"
+    fi
+  fi
+}
+
+if [ -n "$1" ] ; then
+  RESPONSE=$1
+  rapids_version_check
+  install_RAPIDS
+else
+  echo "As you didn't specify a RAPIDS version, please enter in the box your desired RAPIDS version (ex: '0.$STABLE' or '0.$NIGHTLIES', between 0.$LOWEST to 0.$NIGHTLIES, without the quotes)"
+  echo "and hit Enter. If you need stability, use 0.$STABLE. If you want bleeding edge, use our nightly version (0.$NIGHTLIES), but understand that caveats that come with nightly versions."
+  read RESPONSE
+  rapids_version_check
+  install_RAPIDS
+fi
