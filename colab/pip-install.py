340b77b26311f1e5314736791f8ce9d08e966ca6
Bradley Dice <bdice@bradleydice.com>
2023-12-07 18:42:35 -0600

    Update pip-install.py

--- colab/pip-install.py
+++ colab/pip-install.py
@@ -42,7 +42,7 @@ if ('K80' not in gpu_name):
       break
     else:
       print(line.rstrip())
-  output = subprocess.Popen(["rm -rf /usr/local/lib/python3.8/dist-packages/cupy*"], shell=True, stderr=subprocess.STDOUT, 
+  output = subprocess.Popen(["rm -rf /usr/local/lib/python3.10/dist-packages/cupy*"], shell=True, stderr=subprocess.STDOUT, 
       stdout=subprocess.PIPE)
   for line in io.TextIOWrapper(output.stdout, encoding="utf-8"):
     if(line == ""):
@@ -60,10 +60,10 @@ if ('K80' not in gpu_name):
           ***********************************************************************
           The pip install of RAPIDS is complete.
           
-          Please do not run any further installation from the conda based installation methods, as they may cause issues!  
+          Please do not run any further installation from the conda based installation methods, as they may cause issues!
           
-          Please ensure that you're pulling from the git repo to remain updated with the latest working install scripts. 
-r          
+          Please ensure that you're pulling from the git repo to remain updated with the latest working install scripts.
+
           Troubleshooting:
              - If there is an installation failure, please check back on RAPIDSAI owned templates/notebooks to see how to update your personal files. 
              - If an installation failure persists when using the latest script, please make an issue on https://github.com/rapidsai-community/rapidsai-csp-utils
