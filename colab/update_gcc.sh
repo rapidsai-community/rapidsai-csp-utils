c1b9078ca964816b7423c0159424899cd696a18a
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2023-12-20 13:02:41 -0800

    removed gcc update and upgraded to removing ctk12

--- colab/update_gcc.sh
+++ colab/update_gcc.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 echo "Updating your Colab environment.  This will restart your kernel.  Don't Panic!"
 pip install -q condacolab
-pip uninstall -y cupy-cuda11x
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-echo "Added repo"
-sudo apt-get update
-echo "Installing libstdc++"
-sudo apt-get -y install libstdc++6=11.1.0*
+pip uninstall -y cupy-cuda12x
 echo "restarting Colab..."
