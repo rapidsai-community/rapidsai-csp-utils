c95398231ff7f6a3cdeaf08f121025517e5b7c40
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2024-02-14 20:17:43 -0800

    Update install_rapids.py for pascal deprecation and compatibility remediation

--- colab/install_rapids.py
+++ colab/install_rapids.py
@@ -3,6 +3,29 @@ import os, sys, io
 import subprocess
 from pathlib import Path
 
+try: 
+  import pynvml
+except:
+  output = subprocess.Popen(["pip install pynvml"], shell=True, stderr=subprocess.STDOUT, 
+      stdout=subprocess.PIPE)
+  for line in io.TextIOWrapper(output.stdout, encoding="utf-8"):
+    if(line == ""):
+      break
+    else:
+      print(line.rstrip())
+  import pynvml
+try:
+  pynvml.nvmlInit()
+except:
+  raise Exception("""
+                  Unfortunately you're in a Colab instance that doesn't have a GPU.
+
+                  Please make sure you've configured Colab to request a GPU Instance Type.
+               
+                  Go to 'Runtime -> Change Runtime Type --> under the Hardware Accelerator, select GPU', then try again."""
+  )
+gpu_name = pynvml.nvmlDeviceGetName(pynvml.nvmlDeviceGetHandleByIndex(0))
+
 # CFFI fix with pip 
 output = subprocess.Popen(["pip uninstall --yes cffi"], shell=True, stderr=subprocess.STDOUT, 
     stdout=subprocess.PIPE)
@@ -33,14 +56,18 @@ for line in io.TextIOWrapper(output.stdout, encoding="utf-8"):
   else:
     print(line.rstrip())
 
-# Install RAPIDS
+# Install RAPIDS, overriding install version if a Pascal GPU is found
 pkg = "rapids"
-if(sys.argv[1] == "nightly"):
-  release =  ["rapidsai-nightly", "24.02"]
-  print("Installing RAPIDS Nightly "+release[1])
-else:
-  release = ["rapidsai", "23.12"]
-  print("Installing RAPIDS Stable "+release[1])
+if ('P' not in gpu_name): # Currently supported GPU
+    if(sys.argv[1] == "nightly"):
+      release =  ["rapidsai-nightly", "24.04"]
+      print("Installing RAPIDS Nightly "+release[1])
+    else:
+      release = ["rapidsai", "24.02"]
+      print("Installing RAPIDS Stable "+release[1])
+else: # Pascal GPU Installation options (Not currently supported)
+  release =  ["rapidsai", "23.12"]
+  print(f"Installing RAPIDS compatible with your Pascal GPU, a {gpu_name}, "+release[1])
 
 pkg = "rapids"
 print("Starting the RAPIDS install on Colab.  This will take about 15 minutes.")
