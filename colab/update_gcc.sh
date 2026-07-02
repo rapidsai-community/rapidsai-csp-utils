d201f371ea941391090a5cbfae2e89555e8aeb64
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2026-07-02 16:38:01 -0700

    Add SPDX headers to shell and C++ files
    Signed-off-by: Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>

--- colab/update_gcc.sh
+++ colab/update_gcc.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 echo "Updating your Colab environment.  This will restart your kernel.  Don't Panic!"
 pip install -q condacolab
 pip uninstall -y cupy-cuda12x
